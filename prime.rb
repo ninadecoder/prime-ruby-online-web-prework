0f88ecf59bb291c3bb7b706da17d63d3f9b71ed4
Nina Kazzim <kazzim@icloud.com>
2019-05-30 02:54:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(int)
+  
+ if int <= 3
+   return TRUE
+ elsif int % 2== 0 || int % 3 == 0
+  return FALSE
+ 
+ i = 5
+ while i * i <= int
+  if int % i || int % (i + 2) == 0
+    return FALSE
+  i += 1 
+ return TRUE
+    
+end
+
